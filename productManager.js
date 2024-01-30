bd1195274467d0d4a964698d9155fcdfcef97c8b
Nicolas Martin Reynoso <nico97lp@hotmail.com>
2024-01-29 22:26:31 -0300

    desafio2

--- productManager.js
+++ productManager.js
@@ -1,13 +1,17 @@
+import fs from "fs"
+
+
+const PATH = './products.json'
 class ProductManager {
     constructor() {
-        this.products = []
+        this.path = PATH
     }
 
-    addProduct(title, description, price, thumbnail, stock, code) {
+    async addProduct(title, description, price, thumbnail, stock, code) {
         let newcode = code
-        let products = this.products
+        let products = JSON.parse(await fs.promises.readFile(this.path, "utf-8"))
         if (products.find((product) => product.code === newcode)) {
-            console.log("Error = Codigo de producto repetido")
+            console.log(`Error = Codigo ${newcode}de producto repetido`)
         } else {
             const product = {
                 nombre: this.title = title,
@@ -15,43 +19,86 @@ class ProductManager {
                 precio: this.price = price,
                 imagen: this.thumbnail = thumbnail,
                 stock: this.stock = stock,
-                id: this.#idCreator() + 1,
-                code: this.code=code
+                id: this.#idCreator(products) + 1,
+                code: this.code = code
             }
             products.push(product)
+            await fs.promises.writeFile(this.path, JSON.stringify(products))
         }
     }
-    #idCreator() {
+
+    #idCreator(products) {
         let id = 0
-        const products = this.products
         products.map((product) => {
             if (product.id > id) id = product.id
         })
         return id
     }
-
-    getProducts() {
-        const products = this.products
-        console.log(products)
-    }
-
-    getProductsById(id) { 
-        const products = this.products
-        const productFilter = products.find((product) => product.id === id)
-        if (productFilter) {
-            console.log(productFilter)
+    async getProducts() {
+        let products = []
+        if (fs.existsSync(this.path)) {
+            let data = await fs.promises.readFile(this.path, 'utf-8')
+            let products = JSON.parse(data)
+            console.log(products)
         } else {
-            console.log("No se encuentra el producto con el id ingresado")
+            await fs.promises.writeFile(this.path, JSON.stringify(products))
+            console.log(products)
         }
     }
 
+
+    async getProductById(id) {
+        let data = await fs.promises.readFile(this.path, 'utf-8')
+        let products = JSON.parse(data)
+        let productFilter = products.find((product) => product.id === id)
+        if (productFilter) {
+            console.log(productFilter)
+        } else {
+            console.log(`Error:no se encuentra el producto con el id (${id})`)
+        }
+    }
+
+    async updateProduct(id, changeProduct) {
+        let data = await fs.promises.readFile(this.path, 'utf-8')
+        let products = JSON.parse(data)
+        const product = products.find((product) => product.id === id)
+        let productFinder = products.findIndex((product) => product.id === id)
+        if (productFinder != -1) {
+            products[productFinder] = {
+                ...product,
+                ...changeProduct
+            }
+            await fs.promises.writeFile(this.path, JSON.stringify(products))
+        }
+        else {
+            console.log(`Error:no se encuentra el producto con el id (${id})`)
+        }
+
+    }
+
+    async deleteProductById(id) {
+        let data = await fs.promises.readFile(this.path, 'utf-8')
+        let products = JSON.parse(data)
+        let productFilter = products.find((product) => product.id === id)
+
+        if (productFilter) {
+            let newlist = products.filter((product) => product.id !== id)
+            await fs.promises.writeFile(this.path, JSON.stringify(newlist))
+        } else {
+            console.log(`Error:no se encuentra el producto con el id (${id})`)
+        }
+}
 }
 
+
+// Proceso de testing //
+
 const prueba = new ProductManager()
-prueba.getProducts()
-prueba.addProduct("producto de prueba", "Este es un producto prueba", 200, "sin imagen", 20, "abc123")
-prueba.getProducts()
-prueba.addProduct("producto de prueba", "Este es un producto prueba", 200, "sin imagen", 20, "abc125")
-prueba.getProducts()
-prueba.addProduct("producto de prueba", "Este es un producto prueba", 200, "sin imagen", 20, "abc123")
-prueba.getProductById(2)
+await prueba.getProducts()
+await prueba.addProduct("producto de prueba", "Este es un producto prueba", 200, [], 20, "abc123")
+await prueba.getProducts()
+await prueba.getProductById(2)
+await prueba.addProduct("producto de prueba", "Este es un producto prueba", 200, [], 20,"abc456")
+await prueba.getProductById(2)
+await prueba.updateProduct(1,{nombre:"producto actualizado"})
+await prueba.deleteProductById(2)
